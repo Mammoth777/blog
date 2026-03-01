65ccc48e463895abe2fbbc3daef57f215e2133bb
jachy <mammoth77@qq.com>
2026-03-01 21:45:02 +0800

    vault backup: 2026-03-01 21:45:02

---
+++ custom-web/src/views/HaHaJianpu.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <main class="page">
+    <iframe src="https://mammoth777.github.io/hh-jianpu/" frameborder="0"></iframe>
+  </main>
+</template>
+
+<style scoped>
+.page>iframe {
+  width: 100%;
+  height: 80vh;
+  border: none;
+}
+</style>
